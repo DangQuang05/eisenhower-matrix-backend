f4ca2e2b747edeb9ffafdd082df949c62d63225b
DangQuang05 <trinhdangquang05@gmail.com>
2026-08-26 19:44:30 +0700

    first commit

---
+++ src/main/java/com/example/eisenhower_matrix_backend/EisenhowerMatrixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.eisenhower_matrix_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EisenhowerMatrixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EisenhowerMatrixBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eisenhower_matrix_backend/EisenhowerMatrixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eisenhower_matrix_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EisenhowerMatrixBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
